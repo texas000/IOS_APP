ded630f5f251c8d6495471cf0fa9736f3eebacbb
SMARTJIN <texas000@hotmail.com>
2019-11-30 16:20:17 -0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by JIN KIM on 11/30/19.
+//  Copyright © 2019 JINNY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
