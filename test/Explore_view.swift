98d3a3f4468005fcd4479604e017d33b0f666a26
SMARTJIN <texas000@hotmail.com>
2019-12-12 00:23:08 -0800

    explore page - detail view and display the map in the current location

--- test/Explore_view.swift
+++ test/Explore_view.swift
@@ -57,7 +57,6 @@ struct Explore_view: View {
                         }
                         
                         Spacer()
-                        
                     
                         Image(i.prof)
                             .resizable()
@@ -67,6 +66,7 @@ struct Explore_view: View {
                             .overlay(Circle().stroke(Color.gray, lineWidth: 2))
                             .aspectRatio(contentMode: .fit)
                         Text(i.id)
+                            .fontWeight(.heavy)
                     }.padding()
                 }
             }
@@ -88,12 +88,12 @@ struct Detail : View {
     
     var body: some View{
         VStack{
-            Image("example-room").resizable().frame(width: UIScreen.main.bounds.width, height: 500).aspectRatio(1.35, contentMode: .fill).offset(y: -200).padding(.bottom, -200)
+            Image("rm1").resizable().frame(width: UIScreen.main.bounds.width, height: 500).aspectRatio(1.35, contentMode: .fill).offset(y: -200).padding(.bottom, -200)
             GeometryReader{geo in
                 VStack{
                     Detail_top()
                 }
-            }.background(Color.white)
+            }.background(Color.white) //CANNOT DELETE BC THE SHAPE
             .clipShape(Rounded())
             .padding(.top, -70)
         }
@@ -105,16 +105,37 @@ struct Detail_top : View{
         VStack(alignment: .leading, spacing: 10) {
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Something").fontWeight(.heavy).font(.largeTitle)
-                    Text("Something").fontWeight(.heavy).font(.largeTitle)
+                    Text("ROOM1").fontWeight(.heavy).font(.largeTitle)
+                    Text("로스엔젤레스").fontWeight(.heavy).font(.largeTitle)
                 }
-                
                 Spacer()
-                
                 Text("$299").foregroundColor(Color.gray).font(.largeTitle)
-                
             }
-        }
+            HStack {
+                Image(systemName: "heart")
+                Text("HELLO WORLD").foregroundColor(.blue)
+                Spacer()
+                Image(systemName: "location.circle")
+                Text("LA, CA").foregroundColor(.blue)
+            }
+            
+            Text("Description").fontWeight(.heavy)
+            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.").foregroundColor(.gray)
+
+            HStack (spacing: 8) {
+                Button(action: {
+                    
+                }) {
+                    HStack (spacing: 6){
+                        Text("Book your room")
+                        Image(systemName: "heart")
+                    }.foregroundColor(.white)
+                    .padding()
+                }.background(Color.red)
+                .cornerRadius(8)
+            }.padding(.top, 15)
+        }.padding()
+        
     }
 }
 
@@ -146,12 +167,12 @@ struct MapView : UIViewRepresentable {
 
             //Temporary fix: App crashes as it may execute before getting users current location
 
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.1, execute: {
                 let locValue:CLLocationCoordinate2D = self.manager.location!.coordinate
-                print("CURRENT LOCATION = \(locValue.latitude) \(locValue.longitude)")
-
-                let coordinate = CLLocationCoordinate2D(
-                    latitude: locValue.latitude, longitude: locValue.longitude)
+                //print("CURRENT LOCATION = \(locValue.latitude) \(locValue.longitude)")
+                
+                //let coordinate = CLLocationCoordinate2D(latitude: self.map.centerCoordinate.latitude, longitude: self.map.centerCoordinate.longitude)
+                let coordinate = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
                 let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
                 let region = MKCoordinateRegion(center: coordinate, span: span)
                 uiView.setRegion(region, animated: true)
@@ -170,7 +191,8 @@ struct MapView : UIViewRepresentable {
         manager.requestWhenInUseAuthorization()
         
         //map.centerCoordinate = CLLocationCoordinate2D(latitude: map.centerCoordinate.latitude, longitude: map.centerCoordinate.longitude)
-        //map.setRegion(MKCoordinateRegion(center: map.centerCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)), animated: true)
+        map.centerCoordinate = CLLocationCoordinate2D(latitude: map.centerCoordinate.latitude, longitude: map.centerCoordinate.longitude)
+        map.setRegion(MKCoordinateRegion(center: map.centerCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)), animated: true)
         return map
     }
     
